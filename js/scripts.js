a5b142486d837fce6d8bb320f393937134afb02b
Alex Goodwin <hagoodwi@gmail.com>
2020-08-28 10:29:38 -0700

    add if/else logic to getCost

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 //Business logic
 Pizza.prototype.getCost = function () {
+  //check size
+  if(this.size == "small")  {
+    this.cost = 5;
+  }
+  else if(this.size == "medium")  {
+    this.cost = 10;
+  }
+  else if(this.size == "large") {
+    this.cost = 15;
+  }
+  else if (this.size == "giant")  {
+    this.cost = 30;
+  }
+  //add topping
   if (this.topping == "pepperoni"){
-     this.cost = 10;
+     this.cost += 10;
   }
   else if (this.topping == "cheese"){
-     this.cost = 5;
+     this.cost += 5;
   }
-  else if(this.topping == "artichoke"){
-     this.cost = 15;
+  else if (this.topping == "artichoke") {
+    this.cost += 20;
   }
+  else if (this.topping == "anchovy") {
+    this.cost += 15;
+  }
+  
+
+  
+  
   return this.cost;
 }
 
@@ -24,11 +45,11 @@ function Pizza(topping, size, cost) {
 $(document).ready(function()  {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const topping = $("input#topping1").val();
+    const topping = $("input#topping").val();
     const size = $("input#size").val();
     const cost = 0;
     let pizza = new Pizza (topping, size, cost);
-    pizza.getCost(pizza);
+    alert("Your pizza cost is " + pizza.getCost(pizza));
     
     
 
