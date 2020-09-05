b25fffc64a8595ebba74b5dec30b9effb4112716
Alex Goodwin <hagoodwi@gmail.com>
2020-09-05 08:14:01 -0700

    add resubmission requirements

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 This webpage simulates customer's placing orders at a pizza restaurant. It calculates cost by taking into consideration the toppings and the size of pizza. 
 
+## Specs
+-Should correctly create a pizza object with properties
+-Should correctly return the cost of a user's pizza, based on toppings and pizza size
+-Should correctly add pizza objects to an array
+
+
 ## Setup/Installation Requirements
 
 * Clone pizzaParlor repository.
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-//Business logic
+function Pizza(topping1, topping2, topping3, topping4, topping5, size, cost) {
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
+  this.topping4 = topping4;
+  this.topping5 = topping5;
+  this.size = size;
+  this.cost = cost;
+}
 function PizzaBook(){
   this.pizzas = [];
 }
-
 PizzaBook.prototype.addPizza = function(pizza)  {
   this.pizzas.push(pizza);
 }
-
-
-
-
-
 Pizza.prototype.getCost = function () {
-  //check size
   if(this.size == "Small")  {
     this.cost = 5;
   }
@@ -25,12 +26,11 @@ Pizza.prototype.getCost = function () {
   else if (this.size == "Giant")  {
     this.cost = 30;
   }
-  //add topping
   if (this.topping1 == "Pepperoni"){
-     this.cost += 5;
+    this.cost += 5;
   }
   if (this.topping2 == "Cheese"){
-     this.cost += 5;
+    this.cost += 5;
   }
   if (this.topping3 == "Artichoke") {
     this.cost += 5;
@@ -44,22 +44,9 @@ Pizza.prototype.getCost = function () {
   else {
     this.cost += 0.00;
   }
-  return this.cost;
 }
-
-function Pizza(topping1, topping2, topping3, topping4, topping5, size, cost) {
-  this.topping1 = topping1;
-  this.topping2 = topping2;
-  this.topping3 = topping3;
-  this.topping4 = topping4;
-  this.topping5 = topping5;
-  this.size = size;
-  this.cost = cost;
-}
-
 //ui
 let pizzaBook = new PizzaBook();
-
 $(document).ready(function()  {
   $("#formOne").submit(function(event) {
     event.preventDefault();
@@ -68,24 +55,19 @@ $(document).ready(function()  {
     const Input3 = $("input:checkbox[name=topping3]:checked").val();
     const Input4 = $("input:checkbox[name=topping4]:checked").val();
     const Input5 = $("input:checkbox[name=topping5]:checked").val();
-    
     const size = $("input:radio[name=size]:checked").val();
     const cost = 0;
-    
     let pizza = new Pizza (Input1, Input2, Input3, Input4, Input5, size, cost);
     pizzaBook.addPizza(pizza);
-    $("#cost").text(pizza.getCost(pizza));
+    pizza.getCost();
+    $("#cost").text(pizza.cost);
     $("#output").show();
     $(".alert").show();
     $("#pizzaSize").text(pizza.size);
     console.log(pizzaBook.pizzas);
     $("#orders").text.toString((pizzaBook.pizzas));
-    
-
+  })
 })
-
-})
-
 
 
 
